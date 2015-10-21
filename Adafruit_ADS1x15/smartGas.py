1d994089939c6e4451da1e499fc0a39be403831f
LuisEnVilla <villak@hotmail.es>
2015-10-21 16:52:27 -0500

    dweet

--- Adafruit_ADS1x15/smartGas.py
+++ Adafruit_ADS1x15/smartGas.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import time, signal, sys
+import dweepy
 from Adafruit_ADS1x15 import ADS1x15
 
 def signal_handler(signal, frame):
@@ -9,30 +10,12 @@ signal.signal(signal.SIGINT, signal_handler)
 
 ADS1015 = 0x00  # 12-bit ADC
 ADS1115 = 0x01	# 16-bit ADC
-
-# Select the gain
-# gain = 6144  # +/- 6.144V
 gain = 4096  # +/- 4.096V
-# gain = 2048  # +/- 2.048V
-# gain = 1024  # +/- 1.024V
-# gain = 512   # +/- 0.512V
-# gain = 256   # +/- 0.256V
-
-# Select the sample rate
-# sps = 8    # 8 samples per second
-# sps = 16   # 16 samples per second
-# sps = 32   # 32 samples per second
-# sps = 64   # 64 samples per second
-# sps = 128  # 128 samples per second
 sps = 250  # 250 samples per second
-# sps = 475  # 475 samples per second
-# sps = 860  # 860 samples per second
-
-# Initialise the ADC using the default mode (use default I2C address)
-# Set this to ADS1015 or ADS1115 depending on the ADC you are using!
 adc = ADS1x15(ic=ADS1115)
 
 while True:
 	time.sleep(0.5)
 	volts = adc.readADCSingleEnded(0, gain, sps) / 1000
+	dweepy.dweet_for('smartgas', {'Gas': volts})
 	print "%.6f" % (volts)
