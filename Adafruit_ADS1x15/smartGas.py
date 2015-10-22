c8b73fb11f85b97e637f1b45cc722f1adaf25cb3
root <root@raspberrypi.(none)>
2015-10-22 01:07:41 +0100

    freeboard

--- Adafruit_ADS1x15/smartGas.py
+++ Adafruit_ADS1x15/smartGas.py
@@ -14,12 +14,17 @@ ADS1115 = 0x01	# 16-bit ADC
 gain = 4096  # +/- 4.096V
 sps = 250  # 250 samples per second
 adc = ADS1x15(ic=ADS1115)
-
+msgEnviado = False
 while True:
-	time.sleep(1)
+	time.sleep(0.1)
 	volts = adc.readADCSingleEnded(0, gain, sps) / 1000
-	dweepy.dweet_for('smartgas', {'Gas': volts})
-	if (volts<=1):
-		os.system("python ../AlertWhats/run.py "+"5217721199947 "+ "'No hay gas...'")
-		os.system("python ../AlertWhats/run.py "+"5213331676227 "+ "'No hay gas...'")
+	percent = int((volts*100)/3.5)
+	dweepy.dweet_for('smartgasIoTRaspi', {'Gas': percent,'Volts':volts, 'sms':msgEnviado})
+	if (volts<=1 and msgEnviado==False):
+		msgEnviado = True
+		os.system("python ../AlertWhats/run.py "+"5217721199947 "+ "'No hay gas...' &")
+		#os.system("python ../AlertWhats/run.py "+"5213331676227 "+ "'No hay gas...'")
+	if(volts>1):
+		msgEnviado = False
 	print "%.6f" % (volts)
+	print percent
